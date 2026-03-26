eef9a7f39157b3d84de39002798a4a2848136efa
Zara <duruzara4@gmail.com>
2026-03-26 15:00:08 +0100

    front end for other pages besides home, removed unused code

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PORT = 3000
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.use(express.static("public"))
+
 // Connect to MongoDB (Connect once, not inside function)
 mongoose.connect(
 'mongodb+srv://Duru-Zara-DB:EQWAsU26HhspVqTq@cluster0.4cwtf68.mongodb.net/?appName=Cluster0'
@@ -34,19 +36,16 @@ app.post("/api/submit-cat", async (req, res) => {
     try {
         const newCat = new Kitten({
             name: req.body.catName
-        })
+        });
 
-        await newCat.save()
+        await newCat.save();
 
-        res.json({
-            msg: "Cat saved successfully",
-            cat: newCat
-        })
+        res.redirect("/view-cats.html");
 
     } catch (error) {
-        res.status(500).json({ error: error.message })
+        res.status(500).json({ error: error.message });
     }
-})
+});
 
 
 //Read
@@ -99,78 +98,9 @@ app.delete("/api/delete-cat/:id", async (req, res) => {
     }
 })
 
-app.get("/", (req, res) => {
-res.send(`
-<h1>Cat API</h1>
-<p>Server is working!</p>
-
-<form action="/api/submit-cat" method="POST">
-<input type="text" name="catName" placeholder="Enter Cat Name"/>
-<button type="submit">Add Cat</button>
-</form>
-
-<br>
-
-<a href="/api/cats">View All Cats</a>
-`)
-})
 
 //start
 app.listen(PORT, () => {
     console.log("API is listening on Port:", PORT)
 })
-
-/* const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-const PORT = 3000
-const errorMSG = {}
-const status = {}
-app.use(express.urlencoded({extended:true}))
-app.get("/api/server/status", (req,res) =>{
-status.msg = "Server is up and ready";
-res.json(status)
-})
-app.post("/api/submit-cat", async(req,res) => {
-main(res.req.body.catName)
-})
-app.listen(PORT, ()=> {
-console.log("API is listening on Port: ", PORT)
-})
-async function main(kittenName) {
-await mongoose.connect('mongodb+srv://Duru-Zara-DB:EQWAsU26HhspVqTq@cluster0.4cwtf68.mongodb.net/?appName=Cluster0')
-
-const kittySchema = new mongoose.Schema({name:String})
-const kitten = new mongoose.model('Kitten', kittySchema)
-const kitty1 = new kitten({name: kittenName})
-console.log(kitty1.name)
-kitty1.save()
-}*/
-
-// main()
-
-/* const express = require('express'); //import module
-const app = express(); //creating a constructor
-const PORT = 3000;
-
-app.get("/hello-api", (req, res)=>{
-    res.send("Hello World, welcome to JS backend programming!")
-});
-
-app.get("/hello-api-json", (req, res)=>{
-    res.send({greetings:"Hello world!, from backend and server side!"})
-});
-
-app.listen(PORT, ()=>{
-    console.log("Hello world API app listening on port "+PORT)
-})
-
-const errorMsg={};
-const status={};
-app.use(express.urlencoded({ extended: true}))
-app.get("/api/server/status", async(req, res) =>{
-    const query = req.query;
-    status.msg =""
-    //abeg complete ts later
-}) */
 
